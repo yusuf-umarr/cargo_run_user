a91480879c1982fc2e339c948676d02ea97753d4
yusuf <76850966+yusuf-umarr@users.noreply.github.com>
2024-11-18 13:49:44 +0100

    update: screen potriate

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,27 @@ import 'dart:async';
 import 'package:cargo_run/services/notification_service.dart';
 import 'package:cargo_run/services/service_locator.dart';
 import 'package:flutter/material.dart';
-
 import 'cargo_run.dart';
 import 'utils/shared_prefs.dart';
+import 'services/service_locator.dart';
 
-Future<void> main() async {
+void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  await NotificationService.initializeNotification();
+    WidgetsFlutterBinding.ensureInitialized();
   await sharedPrefs.init();
-  setupServiceLocator();
-  runApp(CargoRun(   usedApp: sharedPrefs.usedApp));
+  await NotificationService.initializeNotification();
+   setupServiceLocator();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp, 
+  ]).then((_) {
+    runApp(CargoRun(   usedApp: sharedPrefs.usedApp));
+  });
 }
+
+// Future<void> main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await NotificationService.initializeNotification();
+//   await sharedPrefs.init();
+//   setupServiceLocator();
+//   runApp(CargoRun(   usedApp: sharedPrefs.usedApp));
+// }
